979225c75f9b562884e16599055b56804872889a
ahmet.uygun <ygnhmt@gmail.com>
2025-06-07 12:39:55 +0100

    listing events in the main page

--- src/main/webapp/app/layouts/main/main.component.ts
+++ src/main/webapp/app/layouts/main/main.component.ts
@@ -7,12 +7,13 @@ import { AccountService } from 'app/core/auth/account.service';
 import { AppPageTitleStrategy } from 'app/app-page-title-strategy';
 import FooterComponent from '../footer/footer.component';
 import PageRibbonComponent from '../profiles/page-ribbon.component';
+import { EventComponent } from 'app/entities/event/list/event.component';
 
 @Component({
   selector: 'jhi-main',
   templateUrl: './main.component.html',
   providers: [AppPageTitleStrategy],
-  imports: [RouterOutlet, FooterComponent, PageRibbonComponent],
+  imports: [RouterOutlet, FooterComponent, PageRibbonComponent, EventComponent],
 })
 export default class MainComponent implements OnInit {
   private readonly renderer: Renderer2;
