4b507edbc92c50f61c76e1011c683d7e6f47ff48
ahmet.uygun <ygnhmt@gmail.com>
2025-06-07 12:41:17 +0100

    make the api public

--- src/main/java/com/meet/singles/config/SecurityConfiguration.java
+++ src/main/java/com/meet/singles/config/SecurityConfiguration.java
@@ -88,6 +88,7 @@ public class SecurityConfiguration {
                     .requestMatchers(mvc.pattern("/swagger-ui/**")).permitAll()
                     .requestMatchers(mvc.pattern("/api/authenticate")).permitAll()
                     .requestMatchers(mvc.pattern("/api/auth-info")).permitAll()
+                    .requestMatchers(mvc.pattern("/api/events")).permitAll()
                     .requestMatchers(mvc.pattern("/api/admin/**")).hasAuthority(AuthoritiesConstants.ADMIN)
                     .requestMatchers(mvc.pattern("/api/**")).authenticated()
                     .requestMatchers(mvc.pattern("/v3/api-docs/**")).hasAuthority(AuthoritiesConstants.ADMIN)
