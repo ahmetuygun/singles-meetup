1073fefa169df77b5f81980bd0215dc21c7a6c82
ahmet.uygun <ygnhmt@gmail.com>
2025-06-08 20:32:01 +0100

    I havr already account routes login

--- src/main/webapp/app/landing/landing.component.ts
+++ src/main/webapp/app/landing/landing.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoginService } from 'app/login/login.service';
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +8,13 @@ import { Router } from '@angular/router';
   templateUrl: './landing.component.html',
 })
 export class LandingComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private loginService: LoginService) {}
 
   goToTest(): void {
     this.router.navigate(['/questionnaire']);
+  }
+
+  goToLogin(): void {
+    this.loginService.login();
   }
 } 
