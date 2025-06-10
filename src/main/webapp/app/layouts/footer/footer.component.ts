8ee73d261b086c3c2a7a46c10006c3e5010c1168
ahmet.uygun <ygnhmt@gmail.com>
2025-06-10 12:49:11 +0100

    footer

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NgIf } from '@angular/common';
 import { TranslateDirective } from 'app/shared/language';
+import { AccountService } from 'app/core/auth/account.service';
 
 @Component({
   selector: 'jhi-footer',
   templateUrl: './footer.component.html',
-  imports: [TranslateDirective],
+  styleUrl: './footer.component.scss',
+  imports: [TranslateDirective, RouterModule, NgIf],
 })
-export default class FooterComponent {}
+export default class FooterComponent {
+  currentYear = new Date().getFullYear();
+  
+  private accountService = inject(AccountService);
+
+  isAuthenticated(): boolean {
+    return this.accountService.isAuthenticated();
+  }
+
+  get account() {
+    return this.accountService.trackCurrentAccount()();
+  }
+
+  scrollToHowItWorks(): void {
+    const element = document.querySelector('.info-boxes-wrapper');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+
+  scrollToEvents(): void {
+    const element = document.getElementById('events-section');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+}
